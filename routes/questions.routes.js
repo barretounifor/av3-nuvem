e315c3a4aa21a619b4b6eadd5a6e39e5918004bb
ArthurMontMorency <115747120+ArthurMontMorency@users.noreply.github.com>
2022-11-13 16:46:28 -0300

    rotas 2-4

--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -6,4 +6,22 @@ questionsRoute.get("/questions/1", (req, res) => {
     return res.json(firstQuestion)
 })
 
+questionsRoute.get("/questions/2", (req, res) => {
+    const secondQuestion = require('../question/2st-question.json');
+
+    return res.json(secondQuestion)
+})
+
+questionsRoute.get("/questions/3", (req, res) => {
+    const thirdQuestion = require('../question/3st-question.json');
+
+    return res.json(thirdQuestion)
+})
+
+questionsRoute.get("/questions/4", (req, res) => {
+    const fourthQuestion = require('../question/4st-question.json');
+
+    return res.json(fourthQuestion)
+})
+
 module.exports = questionsRoute
