bb873aeebcf1c3ab8b3dfb412a1e9c4becf21a1f
Lucas Barreto <lucas_barreto29@outlook.com>
2022-11-20 18:15:00 -0300

    adicionado cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const allRoutes = require('./routes/allRoutes.routes');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json())
+app.use(cors())
 app.use('/images', express.static('./herois_fotos'))
 app.use(allRoutes)
 
